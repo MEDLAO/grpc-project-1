3623da81864801ad3a5d86e55d2c7579b927f33f
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-11-06 18:33:46 +0100

    Fix missing MathService instance issue in server registration

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import math_pb2_grpc
 
 # implements the MathService defined in the .proto file
 class MathService(math_pb2_grpc.MathServiceServicer):
-    def square(self, request, context):
+    def Square(self, request, context):
         result_value = request.number * request.number
         return math_pb2.SquareResponse(result=result_value)
 
@@ -17,7 +17,7 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor())
 
     # register the MathService implementation to this gRPC server
-    math_pb2_grpc.add_MathServiceServicer_to_server(MathService, server)
+    math_pb2_grpc.add_MathServiceServicer_to_server(MathService(), server)
 
     # server will listen on port 50051 for incoming gRPC connections
     server.add_insecure_port("[::]:50051")
